5769aa7af0b2da6c39f5bec0af814e5c7252c315
jaminglam <jaminglam@msn.cn>
2016-03-16 08:02:16 +0000

    KDTree prototype finished

--- KDTree.py
+++ KDTree.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 class KDNode:
     def __init__(self, val):
         self.val = val
@@ -9,6 +10,9 @@ class KDNode:
     def setRight(self, right):
         self.right = right
 
+    def setAxis(self, axis):
+        self.axis = axis
+
 class KDTree:
     """
        @param Numpy.Matrix dataset
@@ -16,8 +20,11 @@ class KDTree:
     def __init__(self, dataset):
         self.root = self.buildTree(dataset, 0)
         
-    def dist(self, x,y):   
+    def dist(self, x, y):   
         return np.sqrt(np.sum((x-y)**2))
+
+    def node_dist(self, node1, node2):
+        return self.dist(node1.val, node2.val)
 
     def buildTree(self, dataset, depth):
         if dataset is None:
@@ -37,6 +44,7 @@ class KDTree:
                 break
         node_val = dataset[domain_row_index, :].A1
         node = KDNode(node_val)
+        node.setAxis(axis)
         left_list = []
         right_list = []
         for i in range(0, m):
@@ -59,26 +67,41 @@ class KDTree:
         node.setRight(self.buildTree(right_dataset, depth+1))
         return node
 
-    def searchNearest(self, node, x, depth):
-        if (node.left == None and node.right == None):
-            nearest = node
-            return nearest
+    def nn(self, node, x_node, depth, nearest, min_dist):
+        if node == None:
+            return None
+        if (node != None and node.left == None and node.right == None):
+            dist = self.node_dist(x_node, node)
+            if (dist <= min_dist):
+                return node, dist
+            else:
+                return nearest, min_dist
+        cur_dist = self.node_dist(node, x_node)
+        if (x_node.val[node.axis] < node.val[node.axis]):
+            first = "left"
+            first_node, sec_node = node.left, node.right
         else:
-            axis = depth % n
-            if (x[axis] < node.val[axis] and node.left != None):
-                nearest = self.searchNearest(node.left, x, depth+1)
-                if (self.dist(node.val, x) < self.dist(nearest.val, x)):
-                    nearest = node
-                if (node.right != None and self.dist(node.right.val, x) <= self.dist(nearest.val, x)):
-                    nearest = self.searchNearest(node.right, x, depth+1)
-                return nearest
-            elif (x[axis] >= node.val[axis] and node.right != None):
-                nearest = self.searchNearest(node.right, x, depth+1)
-                if (self.dist(node.val, x) < self.dist(nearest.val, x)):
-                    nearest = node
-                if (node.left != None and self.dist(node.left.val, x) <= self.dist(nearest.val, x)):
-                    nearest = self.searchNearest(node.right, x, depth+1)
-                return nearest
+            first = "right"
+            first_node, sec_node = node.right, node.left
+        if (cur_dist <= min_dist):
+            nearest = node
+            min_dist = cur_dist
+        if (first_node != None):
+            new_nearest, new_min_dist = self.nn(first_node, x_node, depth+1, nearest, min_dist)
+            if (new_min_dist <= min_dist):
+                nearest = new_nearest
+                min_dist = new_min_dist
+        if (first == "left" and x_node.val[node.axis] + min_dist >= node.val[node.axis] and sec_node != None):
+            new_nearest, new_min_dist = self.nn(sec_node, x_node, depth+1, nearest, min_dist)
+            if (new_min_dist <= min_dist):
+                nearest = new_nearest
+                min_dist = new_min_dist
+        elif (first == "right" and x_node.val[node.axis] - min_dist < node.val[node.axis]):
+            new_nearest, new_min_dist = self.nn(sec_node, x_node, depth+1, nearest, min_dist)
+            if (new_min_dist <= min_dist):
+                nearest = new_nearest
+                min_dist = new_min_dist
+        return nearest, min_dist
 
     """
        traverse is a pre-order traversal
@@ -97,5 +120,9 @@ if __name__ == "__main__":
     dataset = np.matrix([[2.,3.],[5.,4.],[9.,6.],[4.,7.],[8.,1.],[7.,2.]])
     tree = KDTree(dataset)
 
-    visited = tree.traverse(tree.root, [])
-    print visited
+    #visited = tree.traverse(tree.root, [])
+    #print visited
+    x = [8.,2.]
+    x_node = KDNode(x)
+    nearest, min_dist = tree.nn(tree.root, x_node, 0, tree.root, tree.node_dist(tree.root, x_node))
+    print ("nearest %s min_dist %d" %(nearest.val, min_dist))
